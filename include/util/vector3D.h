5cde5173e1108a2cf41e8ed6f2df5f6416437044
Borja Muñoz <borja@MacBook-Air-de-Rodriga.local>
2020-09-23 00:04:56 +0200

    Use smart pointers and optimize methods by making them inline

--- include/util/vector3D.h
+++ include/util/vector3D.h
@@ -5,8 +5,7 @@
 
 #include <math.h>
 
-
-//-------------------------------------------------------------------------------
+#include <memory>
 
 class Vector3D
 {
@@ -41,18 +40,12 @@ class Vector3D
         this->x = this->y = this->z = 0;
     }
 
-
-    //-------------------------------------------------------------------------------
-
     inline Vector3D(real x, real y, real z)
     {
         this->x = x;
         this->y = y;
         this->z = z;
     }
-
-
-    //-------------------------------------------------------------------------------
 
     inline Vector3D(const Vector3D &v)
     {
@@ -61,75 +54,49 @@ class Vector3D
         this->z = v.z;
     }
 
-
-    //-------------------------------------------------------------------------------
-
     ~Vector3D()
     {
 
     }
-
-
-    //-------------------------------------------------------------------------------
 
     inline real getX(void) const
     {
         return(this->x);
     }
 
-
-    //-----------------------------------------------
-
     inline real getY(void) const
     {
         return(this->y);
     }
-
-
-    //-----------------------------------------------
 
     inline real getZ(void) const
     {
         return(this->z);
     }
 
-
-    //-----------------------------------------------
-
     inline void setX(real x)
     {
         this->x = x;
     }
-
-
-    //-----------------------------------------------
 
     inline void setY(real y)
     {
         this->y = y;
     }
 
-
-    //-----------------------------------------------
-
     inline void setZ(real z)
     {
         this->z = z;
     }
 
-    //-----------------------------------------------
-
-    inline real dotProduct(Vector3D *v) const
+    inline real dotProduct(std::shared_ptr<Vector3D> v) const
     {
         return(this->x * v->x + this->y * v->y + this->z * v->z);
     }
 
-
-    //-----------------------------------------------
-
-    inline Vector3D * crossProduct(Vector3D *v) const
+    inline std::shared_ptr<Vector3D> crossProduct(std::shared_ptr<Vector3D> v) const
     {
-        Vector3D *resultado = new Vector3D();
+        std::shared_ptr<Vector3D> resultado = std::make_shared<Vector3D>();
     
         resultado->x = this->y * v->z - this->z * v->y;
         resultado->y = this->z * v->x - this->x * v->z;
@@ -138,15 +105,10 @@ class Vector3D
         return(resultado);
     }
 
-
-    //-----------------------------------------------
-
     inline real length(void) const
     {
         return(sqrt(this->x * this->x + this->y * this->y + this->z * this->z));
     }
-
-    //-----------------------------------------------
 
     inline void normalize(void)
     {
@@ -159,43 +121,27 @@ class Vector3D
         this->z /= length;
     }
 
-
-    //-----------------------------------------------
-
-    inline Vector3D * sum(Vector3D *v) const
+    inline std::shared_ptr<Vector3D> sum(std::shared_ptr<Vector3D> v) const
     {
-        return(new Vector3D(this->x + v->x, 
-                            this->y + v->y, 
-                            this->z + v->z));
+        return(std::make_shared<Vector3D>(this->x + v->x, 
+                                          this->y + v->y, 
+                                          this->z + v->z));
     }
 
-
-    //-----------------------------------------------
-
-    inline Vector3D * substract(Vector3D *v) const
+    inline std::shared_ptr<Vector3D> substract(std::shared_ptr<Vector3D> v) const
     {
-        return(new Vector3D(this->x - v->x, 
-                            this->y - v->y, 
-                            this->z - v->z));
+        return(std::make_shared<Vector3D>(this->x - v->x, 
+                                          this->y - v->y, 
+                                          this->z - v->z));
     }
 
-
-    //-----------------------------------------------
-
-    inline Vector3D * product(real d) const
+    inline std::shared_ptr<Vector3D> product(real d) const
     {
-        return(new Vector3D(this->x * d,
-                            this->y * d,
-                            this->z * d));
+        return(std::make_shared<Vector3D>(this->x * d,
+                                          this->y * d,
+                                          this->z * d));
     }
-
-
-    //-----------------------------------------------
-
 
 };
-
-
-//-------------------------------------------------------------------------------
 
 #endif
