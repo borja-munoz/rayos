6b8a7a0957391861f32375df971d44148a331e0c
Borja Muñoz <borja.munoz@gmail.com>
2025-01-02 12:49:29 +0100

    Add render stats

--- include/tracer/tracer.h
+++ include/tracer/tracer.h
@@ -18,11 +18,30 @@
 // Struct to store intersection information
 typedef struct
 {
-	std::shared_ptr<Point3D> hitPoint;     	// Hit Point between Ray and Scene
+	std::shared_ptr<Point3D> hitPoint; 	// Hit Point between Ray and Scene
 	int nearestObject;		       		    // Index for nearest object intersected by the ray
 	std::shared_ptr<Vector3D> normal;		// Object normal in the point intersected by the ray
 } HitPoint;
 
+// Struct to store render stats
+typedef struct {
+  unsigned int numberObjects = 0;
+  unsigned int numberTriangles = 0;
+  unsigned int numSampleRays = 0;
+  unsigned int numShadowRays = 0;
+  unsigned int numRayTriangleTests = 0;
+  unsigned int numRayTriangleIntersections = 0;
+} RenderStats;
+
+// Struct to store execution time measurements
+typedef struct {
+  double timeGetHitPoint = 0;
+  double timeDirectLighting = 0;
+  double timeIndirectLighting = 0;
+  double timeMutuallyVisible = 0;
+  double timeCalculateRadiance = 0;
+  double timePixel = 0;
+} TimeStats;
 
 class Tracer
 {
@@ -36,10 +55,16 @@ class Tracer
 	real (*getRandomNumber)(real x, real y);
 	std::shared_ptr<HitPoint> getHitPoint(std::shared_ptr<Ray> r, std::shared_ptr<Scene> s);
 
+  protected:
+  RenderStats renderStats;
+  TimeStats timeStats;
+
   public:
 	Tracer();
 	Tracer(BRDFtype t);
 	virtual std::shared_ptr<Bitmap> trace(std::shared_ptr<Scene> s) = 0;
+  RenderStats getRenderStats();
+  TimeStats getTimeStats();
 };
 
 
--- src/rayos.cpp
+++ src/rayos.cpp
@@ -3,10 +3,11 @@
 //#include <crtdbg.h>
 
 #include <iostream>
-#include <ctime>   
+#include <ctime>
 #include <sstream>
 #include <string>
 #include <iomanip>
+#include <fstream>
 
 #include <stdio.h>
 #include <math.h>
@@ -20,82 +21,94 @@
 
 std::string getFilename(char *argv[])
 {
-    auto time = std::time(nullptr);
-    std::stringstream ss;
-    ss << std::put_time(std::localtime(&time), "%F_%T"); // ISO 8601 without timezone information.
-    auto strCurrentTime = ss.str();
-    std::replace(strCurrentTime.begin(), strCurrentTime.end(), ':', '-');
+  auto time = std::time(nullptr);
+  std::stringstream ss;
+  ss << std::put_time(std::localtime(&time), "%F_%T"); // ISO 8601 without timezone information.
+  auto strCurrentTime = ss.str();
+  std::replace(strCurrentTime.begin(), strCurrentTime.end(), ':', '-');
 
-    std::stringstream filename;
-    filename << "output/" << argv[1] << "-" << argv[2] << "-" << argv[3] << "-" << strCurrentTime.c_str() << ".tga";
+  std::stringstream filename;
+  filename << "output/" << argv[1] << "-" << argv[2] << "-" << argv[3] << "-" << strCurrentTime.c_str() << ".tga";
 
-    return(filename.str());
+  return (filename.str());
 }
 
 int main(int argc, char *argv[])
 {
-	std::shared_ptr<Scene> s;
-	std::shared_ptr<Bitmap> im;
-	std::shared_ptr<Tracer> rt;
+  std::shared_ptr<Scene> s;
+  std::shared_ptr<Bitmap> im;
+  std::shared_ptr<Tracer> rt;
 
-    // File3ds *f = new File3ds("knot.3ds");
+  // File3ds *f = new File3ds("knot.3ds");
 
-		// Start chrono 
-		Chrono *c = new Chrono();	
-		c->start();
+  // Start chrono
+  Chrono *c = new Chrono();
+  c->start();
 
-		// Instantiate raytracer 
-		if (!strcmp(argv[1], "w"))
-		{
-			// Whitted RayTracer
-			rt = std::make_shared<WhittedRayTracer>(PHONG);
-		}
-		else
-		{
-            // Stochastic RayTracer
-            rt = std::make_shared<StochasticRayTracer>(atoi(argv[2]), atoi(argv[3]));                    
-		}
+  // Instantiate raytracer
+  if (!strcmp(argv[1], "w"))
+  {
+    // Whitted RayTracer
+    rt = std::make_shared<WhittedRayTracer>(PHONG);
+  }
+  else
+  {
+    // Stochastic RayTracer
+    rt = std::make_shared<StochasticRayTracer>(atoi(argv[2]), atoi(argv[3]));
+  }
 
-        // Create scene
-        if (argc == 4)
-        {
-            s = std::make_shared<Scene>();
-        }
-        else
-        {
-            s = std::make_shared<Scene>(argv[4]);
-            return(0);
-        }
-        
-        
-        // Raytrace the scene to create an image
-		im = rt->trace(s);
+  // Create scene
+  if (argc == 4)
+  {
+    s = std::make_shared<Scene>();
+  }
+  else
+  {
+    s = std::make_shared<Scene>(argv[4]);
+    return (0);
+  }
 
-        // Stop chrono
-		c->stop();
-		cout << "Elapsed time = " << c->value() << " seconds\n";
-		delete(c);
+  // Raytrace the scene to create an image
+  im = rt->trace(s);
 
-        // Write the image to a file
-		const char *filename = getFilename(argv).c_str();
-		im->writeTGA(filename);  
-		//im->writeTGA("imagen.tga");  
+  // Stop chrono
+  c->stop();
+  cout << "Elapsed time = " << c->value() << " seconds\n";
+  delete (c);
 
-        std::stringstream openFileCommand;
+  // Write the image to a file
+  std::string strFilename = getFilename(argv);
+  const char *filename = strFilename.c_str();
+  im->writeTGA(filename);
+  // im->writeTGA("imagen.tga");
 
-		// Windows
-        // openFileCommand << "start " << filename;       
-		
-		// Mac OS X
-        openFileCommand << "open " << filename; 
+  std::stringstream openFileCommand;
 
-		system(openFileCommand.str().c_str());	
-	/*
+  // Windows
+  // openFileCommand << "start " << filename;
+
+  // Mac OS X
+  openFileCommand << "open " << filename;
+
+  system(openFileCommand.str().c_str());
+  /*
     }
     else
-		cout << endl << "Usage: rayos [w|s] <sampleRays> <shadowRays> <path_scene_file>" << endl;
+    cout << endl << "Usage: rayos [w|s] <sampleRays> <shadowRays> <path_scene_file>" << endl;
     */
-	return(0);
+
+  // Write time stats to another file
+  strFilename.replace(strFilename.find(".tga"), sizeof(".tga") - 1, ".txt");
+  ofstream o;
+  o.open(strFilename.c_str());
+  o << "Algorithm: " << ((!strcmp(argv[1], "w")) ? "Whitted" : "Stochastic") << "\n";
+  o << "Sample Rays: " << argv[2] << "\n";
+  o << "Shadow Rays: " << argv[3] << "\n";
+  o << "Time Get Hit Point: " << rt->getTimeStats().timeGetHitPoint << " ms\n";  
+  o << "Time Radiance: " << rt->getTimeStats().timeCalculateRadiance << " ms\n";
+  o << "Time Mutually Visible: " << rt->getTimeStats().timeMutuallyVisible << " ms\n";
+  o << "Time Pixel: " << rt->getTimeStats().timePixel << " ms\n";
+  o.close();
+
+  return (0);
 }
-
-
