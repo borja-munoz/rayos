afbaca91b0ca21eeeaa6666737b94f6bf8b98c3f
Borja Muñoz <borja.munoz@gmail.com>
2025-01-26 00:26:41 +0100

    Remove getters and setters

--- src/util/ray.cpp
+++ src/util/ray.cpp
@@ -16,26 +16,6 @@ Ray::~Ray()
 {
 }
 
-Point3D Ray::getOrigin(void) const
-{
-	return(this->origin);  
-}
-
-Vector3D Ray::getDirection(void) const
-{
-	return(this->direction);
-}
-
-void Ray::setOrigin(const Point3D& p)
-{
-	this->origin = p;
-}
-
-void Ray::setDirection(const Vector3D& v)
-{
-	this->direction = v;
-}
-
 // Calculates a point in the ray using the parametric equation
 Point3D Ray::pointParametric(real t) const
 {
